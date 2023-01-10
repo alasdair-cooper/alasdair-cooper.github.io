4e6a259f2653643a5f4c437d9d462cc5b13e1813
alasdair-cooper <alasdair.code@hotmail.com>
2023-01-10 23:23:40 +0000

    Deploying to build from @ alasdair-cooper/alasdair-cooper.github.io@4981dc190daba6993048f520693ccb40b2520b9f 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,8 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
+const CACHE_VERSION = await(await fetch("./env.json")).json();
+
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
@@ -45,6 +47,4 @@ async function onFetch(event) {
     }
 
     return cachedResponse || fetch(event.request);
-}
-
-const CACHE_VERSION = await(await fetch("./env.json")).json();/* Manifest version: fv9Oq+WQ */
+}/* Manifest version: fv9Oq+WQ */
