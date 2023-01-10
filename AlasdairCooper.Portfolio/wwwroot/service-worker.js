227e3c951d2f75a3276d12db8b6ae3c272dec23f
Alasdair Cooper <alasdair.code@hotmail.com>
2023-01-10 20:31:00 +0000

    Updated cache version

--- AlasdairCooper.Portfolio/wwwroot/service-worker.js
+++ AlasdairCooper.Portfolio/wwwroot/service-worker.js
@@ -1,3 +1,5 @@
+const CACHE_VERSION = process.env.GITHUB_RUN_ID;
+
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
